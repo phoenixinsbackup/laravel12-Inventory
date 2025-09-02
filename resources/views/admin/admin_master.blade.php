c8b9db2b301abe6332d7c47e31e4f76b1d9a18e3
Nitiraj <nitish0406@gmail.com>
2025-09-02 17:30:55 +0400

    Brand Crud & Datatable part 2

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -13,6 +13,13 @@
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.ico')}}">
 
+        <!-- Datatables css -->
+        <link href="{{asset('backend/assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('backend/assets/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('backend/assets/libs/datatables.net-keytable-bs5/css/keyTable.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('backend/assets/libs/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('backend/assets/libs/datatables.net-select-bs5/css/select.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+
         <!-- App css -->
         <link href="{{asset('backend/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-style" />
 
@@ -75,6 +82,28 @@
 
         <!-- Widgets Init Js -->
         <script src="{{asset('backend/assets/js/pages/analytics-dashboard.init.js')}}"></script>
+
+        
+       <!-- Datatables js -->
+        <script src="{{asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+
+        <!-- dataTables.bootstrap5 -->
+        <script src="{{asset('backend/assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js')}}"></script>
+        <script src="{{asset('backend/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+
+        
+
+        <!-- buttons.bootstrap5 -->
+        <script src="{{asset('backend/assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js')}}"></script>
+
+       
+
+        
+
+       
+        <!-- Datatable Demo App Js -->
+        <script src="{{asset('backend/assets/js/pages/datatable.init.js')}}"></script>
+
 
         <!-- App js-->
         <script src="{{asset('backend/assets/js/app.js')}}"></script>
